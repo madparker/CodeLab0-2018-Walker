9479286b61b9afece4aea30cab8e17ba61675adb
Matthew Parker <maddparker@gmail.com>
2018-10-26 11:35:29 -0400

    new generated Jetbrains file

---
+++ CodeLab0Walker2018/Assets/Scripts/StartPlatform.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartPlatform : MonoBehaviour
+{
+
+	public Animator animator;
+
+	void OnTriggerEnter(Collider other)
+	{
+		animator.SetBool("StartPlatform", true);
+	}
+}
---
+++ CodeLab0Walker2018/Assets/Scripts/StopPlatform.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class StopPlatform : MonoBehaviour
+{
+
+	public Animator anim;
+
+	void StopAnimation()
+	{
+		anim.SetBool("StartPlatform", false);
+	}
+}
